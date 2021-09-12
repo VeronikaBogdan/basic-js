7fb856cb71c868cd73bf9458d05fc7f6f38432f6
Veronika <veron9399bog@gmail.com>
2021-09-12 03:49:38 +0300

    feat: do 1test arrray

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,12 +1,4 @@
-//import { NotImplementedError } from '../extensions/index.js';
-
 export default function transform(arr) {
-	//let arr = [1, 2, 3, ['qwe','aaa'], 4, 5];
-	//let arr = 3;
-	//console.log(Array.isArray(arr));
-
-
-
 	if (Array.isArray(arr)===true){
 		//let arr = [1, 2, '2', 4, 5];
 		let na = [...arr];
@@ -18,9 +10,9 @@ export default function transform(arr) {
 		else
 		 {
 			 
-			//if(arr.length<3 && arr[0]=='--double-next') return arr;
+			
 
-			if((arr[0]== '--discard-prev' || arr[0]=='--double-prev') && arr.length>2) {arr.shift(); na.shift();}
+			if((arr[0]== '--discard-prev' || arr[0]=='--double-prev') /*&& arr.length>2*/) {arr.shift(); na.shift();}
 			if(arr[arr.length-1]== '--double-next' || arr[arr.length-1]=='--discard-next') {arr.pop();na.pop();}
 
 			for(let i = 0; i < arr.length; i++){     // ТАК ДОЛЖНО БЫТЬ
@@ -46,26 +38,6 @@ export default function transform(arr) {
 					na = arr;
 				}
 			}
-		/*	for(let i = 0; i < arr.length; i++){     // ТАК ДОЛЖНО БЫТЬ
-				if(arr[i+1]=='--discard-prev'){
-					na = arr.slice(k,arr[i-1]);
-					k=arr[i+2]
-					na = [...na,...arr.slice(k,arr.length)]
-				}
-				if(arr[i+1]=='--discard-next'){
-					na = arr.slice(k,arr[i]);
-					k=arr[i+2];
-					na = [...na,...arr.slice(k,arr.length)]
-				}
-				if(arr[i+1]=='--double-next'){
-					arr[i+1] = arr[i+2];
-					na = [...arr];
-				}
-				if(arr[i+1]=='--double-prev'){
-					arr[i+1] = arr[i];
-					na = [...arr];
-				}
-			}*/
 			console.log(na);
 			return na;
 		}
@@ -76,8 +48,4 @@ export default function transform(arr) {
 		return '\'arr\' parameter must be an instance of the Array!';		
 	}
 
-
-
-  //throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
 }
