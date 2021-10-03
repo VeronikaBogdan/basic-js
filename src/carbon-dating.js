1438784718994fa036c4ecba176c38ee7191ee88
Veronika <veron9399bog@gmail.com>
2021-10-03 12:54:35 +0300

    feat: end with Carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,15 +2,8 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 export default function dateSample( sampleActivity ) {
-  
-	if(typeof sampleActivity != 'string'/*|| !Number.isInteger(+sampleActivity)*/|| MODERN_ACTIVITY>15 || +sampleActivity<=0 ) {
+	if( isNaN(sampleActivity) || typeof sampleActivity != 'string' || sampleActivity <= 0 || sampleActivity > MODERN_ACTIVITY ) {
 		return false;
-		// console.log(false);
-	} else
-	{
-		// let k = 0.693/HALF_LIFE_PERIOD;
-		return Math.abs(Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / HALF_LIFE_PERIOD)));
-		// console.log(Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / k));
-	}
-
+	} 
+	return Math.abs(Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD)));
 }
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,30 +1,25 @@
 export default function repeater(str, options) {
-
-	// const options = { separator: 'ds', addition: 'ADD!', additionSeparator: ')))000' }
-	// console.log(options.repeatTimes);
 	let q = '';
 
-	if(typeof str != 'string') str=str+'' /*options[str] = str*/;
-	if(typeof options.addition != 'string') options.addition=options.addition+'' /*options.addition=String(options.addition)*/;
-	
+	if (typeof str != 'string') str += '';
+	if (typeof options.addition != 'string') options.addition += '';
+
 	!options.separator ? options.separator = '+' : options.separator;
-	!options.additionSeparator ? options.additionSeparator = '|': options.additionSeparator ;
+	!options.additionSeparator ? options.additionSeparator = '|' : options.additionSeparator;
 
-	// if(options.repeatTimes === undefined && options.additionRepeatTimes === undefined) q=str+addition ;
-
-	for(let i = 0; i < options.repeatTimes; i++){
+	for (let i = 0; i < options.repeatTimes; i++) {
 		q += str;
-		if(options.addition) {
-			for(let j=0; j<options.additionRepeatTimes; j++){
-				j == options.additionRepeatTimes-1 ? q += options.addition : q += options.addition + options.additionSeparator;
+
+		// options.
+
+		if (options.addition) {
+			for (let j = 0; j < options.additionRepeatTimes; j++) {
+				j == options.additionRepeatTimes - 1 ? q += options.addition : q += options.addition + options.additionSeparator;
 			}
-			//  i==repeatTimes-1 ? q += 
 		}
-		i==options.repeatTimes-1 ? q  : q += options.separator;
+		i == options.repeatTimes - 1 ? q : q += options.separator;
 	}
 
 
-	return options.repeatTimes == undefined &&options.additionRepeatTimes == undefined ? q=str+options.addition:q;
-	// return q;
-	console.log(q);
+	return options.repeatTimes == undefined && options.additionRepeatTimes == undefined ? q = str + options.addition : q;
 }
