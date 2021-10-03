d228f4bc085bac23b2b2617be35a3aa8be7fc28d
Veronika <veron9399bog@gmail.com>
2021-10-03 13:19:57 +0300

    feat: do 1/8 task

--- src/what-season.js
+++ src/what-season.js
@@ -1,22 +1,9 @@
-// import { NotImplementedError } from '../extensions/index.js';
-
-/**
- * Extract season from given date and expose the enemy scout!
- * 
- * @param {Date | FakeDate} date real or fake date
- * @returns {String} time of the year
- * 
- * @example
- * 
- * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
- */
 export default function getSeason(date) {
 
-	
+	if (!date) return 'Unable to determine the time of year!';
+	if ((date instanceof Date) === false) throw new Error('Invalid date!');
 
-	 if(!date) throw new Error('Unable to determine the time of year!');
-	 if((date instanceof Date) === false) throw new Error('Invalid date!');
+  const arrSeasons = ['winter', 'spring', 'summer', 'autumn'];
 
 
 }
